33d3e1e5c3409824d80baa981e7d8fdf31f86910
jwuensche <johannes.wuensche@st.ovgu.de>
2017-11-08 21:13:06 +0100

    fix entropy, add information collect

--- id3.py
+++ id3.py
@@ -45,28 +45,6 @@ def readData(name_of_origin, name_of_destination, attributes):
 #gets index for current entry in regarding array
 def checkEntry(goal, list):
 	#print(goal)
-	global part_unacc
-	global part_acc
-	global part_good
-	global part_vgood
-	global amount
-
-	if goal == 'unacc':
-		amount += 1
-		part_unacc += 1
-
-	if goal == 'acc':
-		amount += 1
-		part_acc += 1
-
-	if goal == 'good':
-		amount += 1
-		part_good += 1
-
-	if goal == 'vgood':
-		amount += 1
-		part_vgood += 1
-
 	for idx,entry in enumerate(list):
 		if entry == goal:
 			return idx
@@ -80,38 +58,58 @@ def checkEntry(goal, list):
 	# return if perfect
 
 # calculates entropy for definig values
-def entropy(stuff):
-	global part_unacc
-	global part_acc
-	global part_good
-	global part_vgood
+def entropy(data_nums):
+# 0 - unacc, 1 - acc, 2 - good, 3 - vgood
 	global amount
-	return (part_unacc / amount) * math.log((part_unacc / amount),4) + (part_acc / amount) * math.log((part_acc / amount),4) + (part_good / amount) * math.log((part_good / amount),4) + (part_vgood / amount) * math.log((part_vgood / amount),4)
+	return (data_nums[0] / amount) * math.log((data_nums[0] / amount),4) - (data_nums[1] / amount) * math.log((data_nums[1] / amount),4) - (data_nums[2] / amount) * math.log((data_nums[2] / amount),4) - (data_nums[3] / amount) * math.log((data_nums[3] / amount),4)
 
-def informationGain():
-	
+#def informationGain():
+
+#what do i actually need? count of things with this argument at attribute, size of list at child, size of list at parent 
+def collectInformation(data):
+	global amount
+	information=[]
+	information.append([0,0,0,0])
+	information.append([0,0,0,0])
+	information.append([0,0,0,0])
+	information.append([0,0,0,0])
+	information.append([0,0,0,0])
+	information.append([0,0,0,0])
+	information.append([0,0,0,0])
+	for list in data:
+		information[0][list[0]]+= 1
+		information[1][list[1]]+= 1
+		information[2][list[2]]+= 1
+		information[3][list[3]]+= 1
+		information[4][list[4]]+= 1
+		information[5][list[5]]+= 1
+		information[6][list[6]]+= 1
+		amount += 1
+
+	return information
 
 #grading values
-values = ['unacc', 'acc', 'good', 'vgood'] #0
+values = ['unacc', 'acc', 'good', 'vgood']
 #decision attributes including grading values
 attributes = [['vhigh', 'high', 'med', 'low'], ['vhigh', 'high', 'med', 'low'], ['2', '3', '4', '5more'], ['2', '4', 'more'], ['small', 'med', 'big'], ['low', 'med', 'high'], ['unacc', 'acc', 'good', 'vgood']]
 #Root creation
 root = Node('node')	
-#Matrix for faster usage
+#Matrix for faster calculation of information gain
 data = []
 #Main working part
 result = open('decision_tree.txt','w+')
 initialize(result)
 original_data = open('cardata/car.data','r')
 data = readData(original_data, data, attributes)
-entropy = entropy('stuff') 
+#get data
+qsave = (collectInformation(data))
 
-print(part_unacc)
-print(part_acc)
-print(part_good)
-print(part_vgood)
-print(amount)
+test = [qsave[6][0],qsave[6][1],qsave[6][2],qsave[6][3]]
+
+entropy = entropy(test)
 
 print(entropy)
+
+#print(entropy)
 original_data.close()
 result.close()
